87ea2e96559bbde27bb293d0c8c6268bfdc47666
lidongheng <419337193@qq.com>
2026-07-12 16:22:35 +0800

    Initial commit

---
+++ ioc/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    这是ioc
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
