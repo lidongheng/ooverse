4a542145fc1697d52281e3dd066c2e6628490eeb
lidongheng <419337193@qq.com>
2026-08-24 16:05:15 +0800

    自写代码part2

---
+++ cost-operation/src/components/home/CommonIndicators.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="item-box">
+    <div class="item-top">
+      <div class="item-top-light" v-if="$slots.light">
+        <slot name="light"></slot>
+      </div>
+      <div class="item-top-num">{{ value }}</div>
+      <div class="item-top-unit">{{ unit }}</div>
+      <slot name="challenge"></slot>
+      <slot name="rightRatio"></slot>
+    </div>
+    <div class="item-bottom">
+      <SvgIcon
+        :icon-name="iconName"
+        :style="{ width: 20, height: 20, color: '#6262a8' }"
+      ></SvgIcon>
+      <div class="item-bottom-label">{{ label }}</div>
+      <RingRatio
+        lable="较上月"
+        class="ring-ratio"
+        :data="ratioValue"
+        :unit="ratioUnit"
+        v-if="!hideRatio"
+        :upGreen="upGreen"
+      >
+        <template #unit v-if="ratioUnit !== '%'">{{ ratioUnit }}</template>
+      </RingRatio>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RingRatio from '@/views/costOperation/component/RingRatio.vue';
+
+const props = defineProps({
+  value: {
+    type: [Number, String],
+  },
+  unit: {
+    type: String,
+  },
+  iconName: {
+    type: String,
+  },
+  label: {
+    type: String,
+  },
+  ratioValue: {
+    type: Number,
+  },
+  ratioUnit: {
+    type: String,
+  },
+  hideRatio: {
+    type: Boolean,
+    default: false,
+  },
+  upGreen: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.item-box {
+  .item-top {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    .item-top-light {
+      margin-right: 10px;
+    }
+    .item-top-num {
+      font-family: Arial;
+      font-size: 40px;
+      font-weight: 700;
+      line-height: 36px;
+      letter-spacing: 0px;
+      text-align: left;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+    }
+    .item-top-unit {
+      font-family: Microsoft YaHei;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 18px;
+      letter-spacing: 0px;
+      text-align: left;
+      margin-left: 4px;
+      margin-top: 12px;
+    }
+    .item-top-target {
+      margin-left: 12px;
+    }
+    .mgl27 {
+      margin-left: 27px;
+    }
+    .item-top-text {
+      border-radius: 10px;
+      background: rgba(113, 90, 251, 1);
+      font-size: 10px;
+      font-weight: 700;
+      line-height: 16px;
+      letter-spacing: 0px;
+      text-align: left;
+      margin-left: 10px;
+      padding: 0px 4px;
+    }
+  }
+  .item-bottom {
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    .item-bottom-icon {
+      width: 16px;
+      height: 16px;
+    }
+    .item-bottom-label {
+      color: rgba(53, 53, 117, 1);
+      font-family: Microsoft YaHei;
+      font-size: 20px;
+      font-weight: 700;
+      height: 28px;
+      line-height: 28px;
+      letter-spacing: 0px;
+      text-align: left;
+      margin-right: 12px;
+      margin-left: 8px;
+    }
+    .ring-ratio {
+      margin-top: 8px;
+    }
+  }
+}
+</style>
--- cost-operation/src/components/home/TopIndicators.vue
+++ cost-operation/src/components/home/TopIndicators.vue
@@ -36,7 +36,9 @@
     <div class="w-419 flex-center">
       <CommonIndicators
         :value="formatRateValue(homoIndicatorsData.assetOnlineRate.curValue)"
-        :ratio-value="formatRateValue(homoIndicatorsData.assetOnlineRate.compareLastMonVal)"
+        :ratio-value="
+          formatRateValue(homoIndicatorsData.assetOnlineRate.compareLastMonVal)
+        "
         label="资产在线率"
         ratio-unit="PCT"
         unit="%"
@@ -59,7 +61,9 @@
       <div class="w-419 flex-center">
         <CommonIndicators
           :value="formatterValue(homoIndicatorsData.service.curValue, 10000)"
-          :ratio-value="formatterValue(homoIndicatorsData.service.compareLastMonVal, 10000)"
+          :ratio-value="
+            formatterValue(homoIndicatorsData.service.compareLastMonVal, 10000)
+          "
           label="服务器"
           ratio-unit="万台"
           unit="万台"
@@ -88,4 +92,238 @@ import {
 } from '../hooks/useIndicators';
 import { useRouter } from 'vue-router';
 import CommonIndicators from './CommonIndicators.vue';
+
+const props = defineProps({
+  isDetailPage: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const chartStyle = {
+  width: 158,
+  height: 98,
+};
+
+const router = useRouter();
+const toIndicatorsPage = () => {
+  if (props.isDetailPage) {
+    return;
+  }
+  router.push({
+    path: '/indicators',
+  });
+};
+
+const colors = ['rgba(98, 98, 168, 1)'];
+
+// 服务器趋势图
+const serviceOptions = computed(() => {
+  const comOp = commonChartOptions();
+  const data = homoIndicatorsData.service.trend ?? [];
+
+  const { data: xAxisData } = comOp.xAxis;
+
+  return {
+    ...comOp,
+    yAxis: {
+      ...comOp.yAxis,
+      name: '万台',
+    },
+    color: colors,
+    series: [
+      {
+        ...comOp.series[0],
+        data: xAxisData.map((v, index) => {
+          const seriesObj = data.find((c) => c.key === v) ?? {};
+          return {
+            value: formatterValue(seriesObj.value, 10000),
+            name: v,
+            ...commonLabelPopover(index, xAxisData.length - 1),
+          };
+        }),
+      },
+    ],
+  };
+});
+
+const assetOptions = computed(() => {
+  const lineValue = formatRateValue(
+    homoIndicatorsData.assetOnlineRate.targetVal,
+  );
+  const comOp = commonChartOptions();
+  const data = homoIndicatorsData.assetOnlineRate.trend ?? [];
+
+  const { data: xAxisData } = comOp.xAxis;
+
+  return {
+    ...comOp,
+    yAxis: {
+      ...comOp.yAxis,
+      name: '%',
+    },
+    color: colors,
+    series: [
+      {
+        ...comOp.series[0],
+        data: xAxisData.map((v, index) => {
+          const seriesObj = data.find((c) => c.key === v) ?? {};
+          return {
+            value: formatRateValue(seriesObj.value, 2),
+            name: v,
+            ...commonLabelPopover(index, xAxisData.length - 1),
+          };
+        }),
+      },
+    ],
+  };
+});
+
+// 总流水
+const totalOptions = computed(() => {
+  const comOp = commonChartOptions();
+  const seriesData = sortTrend(homoIndicatorsData.totalCost.trend ?? []);
+  const { data: xAxisData } = comOp.xAxis;
+  return {
+    ...comOp,
+    yAxis: {
+      ...comOp.yAxis,
+    },
+    color: colors,
+    series: [
+      {
+        ...comOp.series[0],
+        data: xAxisData.map((v, index) => {
+          const seriesObj = seriesData.find((c) => c.axisX === v) ?? {};
+          return {
+            value: toBillion(seriesObj.cost),
+            name: v,
+            ...commonLabelPopover(index, xAxisData.length - 1),
+          };
+        }),
+      },
+    ],
+  };
+});
+
+// 总成本
+const totalCostOptions = computed(() => {
+  const comOp = commonChartOptions();
+  const seriesData = sortTrend(homoIndicatorsData.total.trend ?? []);
+  const { data: xAxisData } = comOp.xAxis;
+
+  return {
+    ...comOp,
+    yAxis: {
+      ...comOp.yAxis,
+      name: '亿元',
+    },
+    color: colors,
+    series: [
+      {
+        ...comOp.series[0],
+        data: xAxisData.map((v, index) => {
+          const seriesObj = seriesData.find((c) => c.axisX === v) ?? {};
+          return {
+            value: toBillion(seriesObj.cost),
+            name: v,
+            ...commonLabelPopover(index, xAxisData.length - 1),
+          };
+        }),
+      },
+    ],
+  };
+});
 </script>
+
+<style lang="less" scoped>
+.value {
+  color: rgba(53, 53, 117, 1);
+  font-size: 40px;
+  font-weight: 500;
+  line-height: 47px;
+  letter-spacing: 0px;
+  text-align: left;
+}
+
+.unit {
+  color: rgba(53, 53, 117, 1);
+  font-family: Microsoft YaHei;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 26px;
+  letter-spacing: 0px;
+  text-align: left;
+}
+
+.flex-center {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.warpper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  color: rgba(53, 53, 117, 1);
+  .w-419 {
+    width: 419px;
+    height: 98px;
+  }
+  .line {
+    width: 1px;
+    height: 96px;
+    background: linear-gradient(
+      180deg,
+      rgba(158, 158, 208, 0) 1%,
+      rgba(158, 158, 208, 1) 51%,
+      rgba(158, 158, 208, 0) 100%
+    );
+  }
+}
+
+.warpper.detail {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding: 20px;
+  color: rgba(53, 53, 117, 1);
+  .w-448 {
+    width: 448px;
+    height: 98px;
+  }
+  .line {
+    width: 1px;
+    height: 96px;
+    margin-left: 144px;
+    margin-right: 48px;
+    background: linear-gradient(
+      180deg,
+      rgba(158, 158, 208, 0) 1%,
+      rgba(158, 158, 208, 1) 51%,
+      rgba(158, 158, 208, 0) 100%
+    );
+  }
+}
+
+.item-top-target {
+  color: rgba(113, 113, 168, 1);
+  font-family: Microsoft YaHei;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: 0px;
+  text-align: left;
+  margin-left: 12px;
+}
+
+.jump-btn {
+  position: absolute;
+  top: 8px;
+  right: 13px;
+}
+</style>
